48fbd1db2b37516b4a7e39f9ab9bf120c25cb607
dmorrow <dmorrow@promodel.com>
2020-01-13 16:07:36 -0500

    Dan's the smartest
    (cherry picked from commit 27be0418c14f40dbb9c7541ae5be51ecbdb45e84)

--- RebaseExample/Program.cs
+++ RebaseExample/Program.cs
@@ -7,7 +7,7 @@ namespace RebaseExample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Dan!");
+            Console.WriteLine("Hello Dan, you're the smartest one here!");
             Console.WriteLine("Hello Josh!");
             Console.WriteLine("Hello Drew!");
             Console.WriteLine("Hello Laif!");
