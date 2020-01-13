9a11d8470cccd3546730ec8dda1b381661a866da
dmorrow <dmorrow@promodel.com>
2020-01-13 16:09:49 -0500

    Dan's the coolest

--- RebaseExample/Program.cs
+++ RebaseExample/Program.cs
@@ -7,7 +7,7 @@ namespace RebaseExample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Dan, you're the smartest one here!");
+            Console.WriteLine("Hello Dan, you're the coolest one here!");
             Console.WriteLine("Hello Josh!");
             Console.WriteLine("Hello Drew!");
             Console.WriteLine("Hello Laif!");
