3b28a8accb1352e6a07e2fe25a2c4d3bccabf7e6
dmorrow <dmorrow@promodel.com>
2020-01-13 16:02:52 -0500

    Hello All

--- RebaseExample/Program.cs
+++ RebaseExample/Program.cs
@@ -7,6 +7,10 @@ namespace RebaseExample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dan!");
+            Console.WriteLine("Hello Josh!");
+            Console.WriteLine("Hello Drew!");
+            Console.WriteLine("Hello Laif!");
         }
     }
 }
