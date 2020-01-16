0fbb713dc609cdeeb7f806e38162ed6bee7cec18
dmorrow <dmorrow@promodel.com>
2020-01-16 15:44:36 -0500

    Dan is the best

--- RebaseExample/Program.cs
+++ RebaseExample/Program.cs
@@ -9,7 +9,7 @@ namespace RebaseExample
             // This is develop, we haven't forgot you emily
             // Haven't forgotten you either Laif
             Console.WriteLine("Hello World, Josh is right!");
-            Console.WriteLine("Hello Dan!");
+            Console.WriteLine("Hello Dan, you're the best!");
             Console.WriteLine("Hello Josh!");
             Console.WriteLine("Hello Drew!");
             Console.WriteLine("Hello Laif!");
